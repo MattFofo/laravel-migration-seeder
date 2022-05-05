a7045442fe45fef5e6fc5e6368d4147d3ed0d814
MattFofo <matteo.segattini2@gmail.com>
2022-05-05 14:46:24 +0200

    creazione tabella trains

---
+++ database/migrations/2022_05_05_122301_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('azienda');
+            $table->string('stazione di partenza');
+            $table->string('stazione di arrivo');
+            $table->time('orario di partenza', $precision = 0);
+            $table->time('orario di di arrivo', $precision = 0);
+            $table->integer('codice treno');
+            $table->tinyInteger('numero di carrozze');
+            $table->boolean('in orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
