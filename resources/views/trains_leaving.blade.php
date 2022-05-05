a05133028aeec8914fbe2b8ad7ae5c1b36ce1cb7
MattFofo <matteo.segattini2@gmail.com>
2022-05-05 16:46:59 +0200

    fix

--- resources/views/trains_leaving.blade.php
+++ resources/views/trains_leaving.blade.php
@@ -7,14 +7,12 @@
         @foreach ($trains as $train)
         <div class="train">
             <h2>{{ $train->azienda }}</h2>
-            <h2>{{ $train->{'stazione di partenza'} }}</h2>
-            <h2>{{ $train->{'stazione di arrivo'} }}</h2>
-
-            <h2>{{ $train->{'orario di partenza'} }}</h2>
-
-            <h2>{{ $train->{'codice treno'} }}</h2>
-
-            <h2>{{ $train->{'numero di carrozze'} }}</h2>
+            <h2><small>stazione di partenza:</small> {{ $train->{'stazione di partenza'} }}</h2>
+            <h2><small>stazione di arrivo:</small> {{ $train->{'stazione di arrivo'} }}</h2>
+            <h2><small>orario di partenza:</small> {{ $train->{'orario di partenza'} }}</h2>
+            <h2><small>orario di arrivo:</small> {{ $train->{'orario di arrivo'} }}</h2>
+            <h2><small>codice treno:</small> {{ $train->{'codice treno'} }}</h2>
+            <h2><small>numero di carrozze:</small> {{ $train->{'numero di carrozze'} }}</h2>
             <h2>{{ $train->{'in orario'} }}</h2>
             <h2>{{ $train->{'cancellato'} }}</h2>
 
