fbef603ad69386588faeee9f97d945565e9992e1
MattFofo <matteo.segattini2@gmail.com>
2022-05-05 17:44:27 +0200

    prove per modificare i nomi delle colonne

--- database/migrations/2022_05_05_132347_update_trains_table.php
+++ database/migrations/2022_05_05_132347_update_trains_table.php
@@ -14,7 +14,13 @@ class UpdateTrainsTable extends Migration
     public function up()
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->renameColumn('stazione di partenza','stazione_di_partenza');
+            $table->renameColumn('stazione di arrivo', 'stazione_di_arrivo');
+            $table->renameColumn('orario di partenza', 'orario_di_partenza');
+            $table->renameColumn('orario di arrivo', 'orario_di_arrivo');
+            $table->renameColumn('codice treno', 'codice_treno');
+            $table->renameColumn('numero di carrozze', 'numero_di_carrozze');
+            $table->renameColumn('in orario', 'in_orario');
         });
     }
 
@@ -26,7 +32,13 @@ class UpdateTrainsTable extends Migration
     public function down()
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->renameColumn('stazione_di_partenza', 'stazione di partenza');
+            $table->renameColumn('stazione_di_arrivo', 'stazione di arrivo');
+            $table->renameColumn('orario_di_partenza', 'orario di partenza');
+            $table->renameColumn('orario_di_arrivo', 'orario di arrivo');
+            $table->renameColumn('codice_treno', 'codice treno');
+            $table->renameColumn('numero_di_carrozze', 'numero di carrozze');
+            $table->renameColumn('in_orario', 'in orario');
         });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,5 @@
 @section('page_title', 'Home')
 
 @section('content')
-
+    <h1>HOMEPAGE</h1>
 @endsection
